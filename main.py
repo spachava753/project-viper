5034563ddcc901ac01059021406ec65f4eb3742a
spmega <spmega4567@gmail.com>
2018-08-02 12:57:36 -0400

    Able to add and remove legitiment symbols, cannot take invalid user input yet

--- main.py
+++ main.py
@@ -3,16 +3,70 @@ from flask import render_template
 from flask import request
 from flask import make_response
 from quote_provider import get_all_quotes
+import datetime
+import random
+import hashlib
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["POST", "GET"])
-def home():
-    symbol = request.form.get("symbol")
-    print(symbol)
-    return make_response(render_template("home.html", quotes=get_all_quotes([symbol])))
+@app.route("/<symbol>", methods=["POST", "GET"])
+def home(symbol=""):
+    if not symbol:
+        print("Add Symbol")
+        symbol = request.form.get("symbol")
+        symbols = request.cookies.get("symbols")
+
+        if symbols:
+            if symbol:
+                symbol = symbol.upper()
+                symbols = symbols.split(',')
+                symbols.append(symbol)
+                symbols = set(symbols)
+        elif symbol:
+            symbols = [symbol]
+        else:
+            symbols = []
+
+        response = make_response(render_template("home.html", quotes=get_all_quotes(symbols)))
+
+        if symbols:
+            cookie_symbols = ','.join(symbols)
+            expires = datetime.datetime.now() + datetime.timedelta(365)
+            response.set_cookie("symbols", cookie_symbols, expires=expires)
+            print(cookie_symbols)
+
+        return response
+    else:
+        print("Remove Symbol")
+        symbols = request.cookies.get("symbols")
+        if symbols:
+            symbol = symbol.upper()
+            if ',' in symbols:
+                symbols = symbols.split(',')
+                symbols.remove(symbol)
+                symbols = set(symbols)
+            else:
+                symbols = []
+
+            response = make_response(render_template("home.html", quotes=get_all_quotes(symbols)))
+
+            if symbols:
+                cookie_symbols = ','.join(symbols)
+                print(cookie_symbols)
+            else:
+                cookie_symbols = ""
+
+            expires = datetime.datetime.now() + datetime.timedelta(365)
+            response.set_cookie("symbols", cookie_symbols, expires=expires)
+        else:
+            response = make_response(render_template("home.html", quotes=get_all_quotes(symbols)))
+
+        return response
 
 
 if __name__ == "__main__":
+    rand_int = str(random.randint(1, 1001))
+    app.secret_key = hashlib.sha256(rand_int.encode()).hexdigest()
     app.run(port=8080, debug=True)
--- quote_provider.py
+++ quote_provider.py
@@ -3,9 +3,12 @@ from quote_config import get_quote
 
 def get_all_quotes(symbols=[]):
     quote_result = []
-    for symbol in symbols:
-        quote_result.append(get_quote(symbol))
+    if symbols:
+        for symbol in symbols:
+            quote_result.append(get_quote(symbol))
+        return quote_result
     return quote_result
+
 
 if __name__ == '__main__':
     print(get_quote("GOOG"))
